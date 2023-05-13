72628f70b31acc21c56e74c26683624a5456777f
yvnji <yvnji25@gmail.com>
2023-05-14 03:24:02 +0900

    Feat: App 레이아웃 구구조 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CommonHeader from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <CommonHeader />
+      <Navigation />
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  max-width: 425px;
+  overflow: hidden;
+  background-color: #fff;
+`;
