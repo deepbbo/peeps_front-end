dbe672a718942160ffa6864287d7513cd890e58b
yvnji <yvnji25@gmail.com>
2023-05-14 03:35:43 +0900

    Design: ResetStyle 설정 및 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import GlobalStyle from './GlobalStyle';
+import ResetStyle from './ResetStyle';
 import reportWebVitals from './reportWebVitals';
 
 // eslint-disable-next-line prettier/prettier
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
+    <ResetStyle />
     <div className="wrap">
       <div className="home-left-area">Peeps</div>
       <App />
