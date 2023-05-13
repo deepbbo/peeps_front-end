55bc388b12350730c5a23b8bf356a47fbf5d69b7
shine-son <schk9611@gmail.com>
2023-05-14 01:26:39 +0900

    Feat: Map 컴포넌트 작성
    - 카카오 지도 API를 활용해 지도 그리기
    - CRA에 내장된 dotenv를 활용해 apikey 환경변수로 만들어 사용하기

--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 
-// kakao를 글로벌로 선언해주어야 앱이 인식할 수 있음
 declare global {
   interface Window {
     kakao: any;
@@ -9,18 +8,29 @@ declare global {
 
 const Map = () => {
   useEffect(() => {
-    window.kakao.maps.load(() => {
-      let mapContainer = document.getElementById('map'); //지도를 담을 영역
-      let mapOptions = {
-        //지도 기본 옵션
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표
-        level: 3 //지도의 레벨(확대, 축소 정도)
-      };
-      let map = new window.kakao.maps.Map(mapContainer, mapOptions); //지도 생성 및 객체 리턴
-    });
+    const apikey: string = process.env.REACT_APP_KAKAO_API_KEY!;
+    // 카카오 지도 API 스크립트를 동적으로 생성하고 로드합니다.
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${apikey}&autoload=false`;
+
+    document.head.appendChild(script);
+
+    // 스크립트가 로드되면 지도를 초기화합니다.
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map'); // 지도를 표시할 div 요소의 id
+        const options = {
+          center: new window.kakao.maps.LatLng(37.5665, 126.978), // 지도의 중심 좌표
+          level: 3 // 지도의 확대 레벨
+        };
+
+        const map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+      });
+    };
   }, []);
 
-  return <div id="map" style={{ width: '500px', height: '400px' }}></div>;
+  return <div id="map" style={{ width: '100%', height: '600px' }}></div>;
 };
 
 export default Map;
