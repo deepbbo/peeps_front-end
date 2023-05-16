34b4b52fa5abe5f5516b775d31407d07437327b0
chaeynii <dlco9292@naver.com>
2023-05-17 04:49:38 +0900

    Feat: Comment, PostCommentInput 컴포넌트 작성

---
+++ src/detail/Comment.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import styles from "./Comment.module.css";
+
+function Comment(){
+
+    // const profileUrl = process.env.PUBLIC_URL+'/icon/profile.svg'
+    
+    return <>
+    {/* <div className={styles.comment_list}> */}
+        {/* {dummy.comments.map((comment) => (
+        <div key={comment.id}  className={styles.comment_item}>
+            <div className={styles.comment_header}>
+                <div style={{"display":"flex"}}>
+                    <img src={comment.profileImgUrl} alt="프로필사진"/>
+                    <span>{comment.author}</span>
+                    <button class="btn_more"><span>더보기버튼ㄴ</span></button>
+                </div>
+            </div>
+            <div className={styles.comment_content}>
+                <p>{comment.text}</p>
+            </div>
+            <div className={styles.comment_footer}>
+                <p>{comment.date}</p>
+            </div> */}
+            
+            
+        {/* </div> */}
+        {/* ))} */}
+    {/* </div> */}
+    </>;
+}
+
+export default Comment;
---
+++ src/detail/PostCommentInput.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function PostCommentInput(){
+    const [inputValue, setInputValue] = useState("");
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+      }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Form submitted with input value:', inputValue);
+    }
+
+    return <>
+    <div>
+        <form onSubmit={handleSubmit}>
+        <label>
+            댓글
+            <input type="text" value={inputValue} onChange={handleChange} />
+        </label>
+        <button type="submit">등록</button>
+        </form>
+    </div>
+    </>
+}
+
+export default PostCommentInput;
