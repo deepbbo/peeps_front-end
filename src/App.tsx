effe8633695c6b3f224e5b1fad11f0f29a818955
yvnji <yvnji25@gmail.com>
2023-05-26 17:19:51 +0900

    Revert "Feature map/review"

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,26 @@
-import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
-import styled from 'styled-components';
-import MapContainer from './components/map/MapContainer';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import ReviewList from './components/review/ReviewList';
-import ReviewWrite from './components/review/ReviewWrite';
-import ReviewDetail from './components/review/ReviewDetail';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <AppContainer>
-      <Header />
-      <Router>
-        <Routes>
-          <Route path="/api/v1/map" element={<MapContainer />}></Route>
-          <Route
-            path="/api/v1/review/location/:location_id"
-            element={<ReviewList />}
-          />
-          <Route path="/api/v1/review" element={<ReviewWrite />} />
-          <Route path="/api/v1/review/:review_id" element={<ReviewDetail />} />
-          <Route path="/board" />
-          <Route path="/chat" />
-          <Route path="/my" />
-        </Routes>
-        <Navigation />
-      </Router>
-    </AppContainer>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
-
-const AppContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100vh;
-  max-width: 425px;
-  overflow: hidden;
-  background-color: #fff;
-`;
